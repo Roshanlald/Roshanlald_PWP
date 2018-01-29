981496cfe469bd22e449a7cdd0b70be842513ea5
Roshanlald <35587615+Roshanlald@users.noreply.github.com>
2018-01-29 16:27:50 +0530

    Add files via upload

---
+++ rr.py
@@ -0,0 +1,102 @@
+import random
+N=0
+while (N<=100):
+    roll=input("enter "r" to roll a dice")
+    if(roll=='r'):
+        r=random.randint(1,6)
+        N=N+r
+        print("you are in position",N)
+        if N==8:
+            N=37
+            print("you are in position",N)
+        elif N==11:
+            N=2
+            print("you are in position",N)
+        elif N==13:
+            N=34
+            print("you are in position",N)
+        elif N==25:
+            N=4
+            print("you are in position",N)
+        elif N==38:
+            N=9
+            print("you are in position",N)
+        elif N==40:
+            N=68
+            print("you are in position",N)
+        elif N==52:
+            N=81
+            print("you are in position",N)
+        elif N==65:
+            N=46
+            print("you are in position",N)
+        elif N==76:
+            N=97
+            print("you are in position",N)
+        elif N==89:
+            N=70
+            print("you are in position",N)
+        elif N==93:
+            N=64
+            print("you are in position",N)
+        else:
+            print("you win")
+              
+                                                     
+                                          
+
+            
+              
+    
+    
+
+    
+    
+
+    
+                      
+               
+                 
+        
+                      
+        
+                          
+                          
+        
+                                
+                                 
+        
+                                     
+                                     
+        
+                                         
+                                          
+        
+                                                
+                                                 
+        
+                                                                         
+   
+
+   
+    
+
+    
+    
+
+  
+
+   
+
+  
+   
+
+    
+    
+
+
+
+
+
+
+
