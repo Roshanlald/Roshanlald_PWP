962adc65a34a54dc0e430219e3c910f6ab6ed38a
Roshanlald <35587615+Roshanlald@users.noreply.github.com>
2018-02-23 09:54:52 +0530

    Add files via upload

---
+++ pro_functions.py
@@ -0,0 +1,19 @@
+#functions
+def add(x,y):
+    return x+y
+def sub(x,y):
+    return x-y
+def mul(x,y):
+    return x*y
+def div(x,y):
+    return x/y
+def flo(x,y):
+    return x//y
+def mod(x,y):
+    return x%y
+print("addition of 10 and 150 is",add(10,150))
+print("subtraction of 10 and 150 is",sub(10,150))
+print("multiplication of 10 and 150 is",mul(10,150))
+print("division of 10 and 150 is",div(10,150))
+print("float division of 10 and 150 is",flo(10,150))
+print("modulo of 10 and 150 is",mod(10,150))
